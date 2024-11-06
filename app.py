bec5aa3750d7bacc5e417edac7e2409c9480124d
Aryan Wadhe <lcs2021049@iiitl.ac.in>
2024-11-07 02:55:33 +0530

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 notes = []
 
 @app.route('/')
@@ -13,13 +14,13 @@ def add_note():
     note_text = request.form['note_text']
     note_color = request.form['note_color']
     notes.append({'text': note_text, 'color': note_color})
-    return index()
+    return render_template('index.html', notes=notes)
 
 @app.route('/delete_note/<int:note_id>', methods=['POST'])
 def delete_note(note_id):
     if 0 <= note_id < len(notes):
         del notes[note_id]
-    return index()
+    return render_template('index.html', notes=notes)
 
 if __name__ == '__main__':
     app.run(debug=True)
